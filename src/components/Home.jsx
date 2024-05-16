586d11495928805dc59a5f312335621017c832ec
Nouditya Badsare <nouditya110703@gmail.com>
2024-05-16 22:21:08 +0530

    5

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -8,13 +8,13 @@ const Home = () => {
     <div name="home" className="h-screen w-full bg-gradient-to-b from-black via-black to-gray-800">
         <div className="max-w-screen-lg mx-auto flex flex-col items-center justify-center h-full px-4 md:flex-row">
             <div className="flex flex-col justify-center h-full">
-                <h2 className="text-4xl sm:text-7xl font-bold text-white">I'm a React Developer</h2>
+                <h2 className="text-4xl sm:text-7xl font-bold text-white">I'm a Full Stack Web Developer</h2>
                 <p className="text-xl text-gray-500 py-4 max-w-md">
                     I am a third year student at Indore Institute of Science and Technology.
                     Currently, I'm pursuing my Bachelor degree in Computer Science.
                     I'm learning to work on web application using technologies like
                     React JS, Tailwind, Bootstrap, Rapid API.
-                    I'm also proficient in programming languages like C, C++, Java, React and JavaScript.
+                    I'm also proficient in programming languages like C, C++, Java and JavaScript.
                 </p>
 
                 <div>
