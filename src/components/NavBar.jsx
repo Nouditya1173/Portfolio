a0f0e5a764da531e8a58d3e3331e333836ed1d35
Nouditya Badsare <nouditya110703@gmail.com>
2025-05-22 11:44:48 +0530

    once again

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -31,7 +31,7 @@ const NavBar = () => {
     
     <div className="flex justify-between items-center w-full h-20 px-4 text-white bg-black fixed">
         <div>
-            <h1 className="text-5xl font-signature ml-2">Nouditya</h1>
+            <h1 className="text-5xl font-signature ml-2">Nouditya Badsare</h1>
         </div>
         
         <ul className="hidden md:flex">
